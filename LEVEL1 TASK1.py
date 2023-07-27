30ddc84d6acfa7e48349210cab142ff27afd8672
vinay kakad <138357161+vinaykakad0728@users.noreply.github.com>
2023-07-28 00:15:12 +0530

    Update LEVEL1 TASK1.py

--- LEVEL1 TASK1.py
+++ LEVEL1 TASK1.py
@@ -3,7 +3,7 @@
 
 
 a=str(input("enter any string"))
-print("BEFORE EXECUTE THE CODE STRING IS:")
+print("BEFORE EXECUTE THE CODE STRING IS: ",str)
 reverse=a[::-1]
 print("AFTER EXECUTE THE CODE THE REVERSE STRING IS:")
 print(reverse)
